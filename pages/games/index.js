2fd53b0f6ad8a14e21884eb581eda4ba85c8a13e
Eric Frey <ericlfrey@gmail.com>
2023-05-26 13:26:05 -0500

    Added a dropdown button to the games page and styled to place on page.

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import GameCard from '../../components/game/GameCard';
 import { getGames } from '../../utils/data/gameData';
@@ -17,13 +17,21 @@ function Home() {
   return (
     <article className="games">
       <h1>Games</h1>
-      <Button
-        onClick={() => {
-          router.push('/games/new');
-        }}
-      >
-        Register New Game
-      </Button>
+      <div className="event-btns">
+        <Button
+          onClick={() => {
+            router.push('/games/new');
+          }}
+        >
+          Register New Game
+        </Button>
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button" className="game-filter-btn">
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
+
+      </div>
       <div className="card-container">
         {games.map((game) => (
           <section key={`game--${game.id}`} className="game">
